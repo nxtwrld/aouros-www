3e20d879fe84d86d803ed21582f98cfcc635438e
Ondrej Masek <nxtwrld@gmail.com>
2023-10-10 17:24:13 +0200

    favicon

--- src/components/segment.svelte
+++ src/components/segment.svelte
@@ -14,7 +14,7 @@
         padding: 1rem;
         width: 110%;
         overflow: hidden;
-        transform: rotate(-2deg) translateX(-5%);
+        transform: rotate(-4deg) translateX(-5%);
         -webkit-mask-image: -webkit-radial-gradient(white, black);
     }
 
@@ -22,7 +22,7 @@
         background-color: var(--color-primary-lighter);
     }
     .segment .page {
-        transform: rotate(2deg);
+        transform: rotate(4deg);
     }
     .segment:nth-child(odd) .page :global(> *) {
         margin-right: 50%;
@@ -48,9 +48,9 @@
     }
 
     .segment:nth-child(even)  .page :global(img) {
-        transform: rotate(2deg) translate(-100%, -25%);
+        transform: rotate(0deg) translate(-100%, -25%);
     }
     .segment:nth-child(odd)  .page :global(img) {
-        transform: rotate(2deg) translate(100%, -25%);
+        transform: rotate(0deg) translate(100%, -25%);
     }
 </style>
