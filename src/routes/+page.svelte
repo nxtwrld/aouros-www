6f83a0ac8eab00d441af575fdcc38abe3474ad6d
Ondrej Masek <nxtwrld@gmail.com>
2024-06-06 15:23:11 +0200

    Home page splash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,39 @@
     
     import { browser } from '$app/environment';
     import { goto } from '$app/navigation';
-    
+    import './www/styles.css';
     if (browser) {
-        goto('/www');
+     //   goto('/www');
     }
 </script>
 
-Redirecting...
+<!-- Redirecting... -->
+
+
+<div class="placer">
+    <img src="/icon.svg" alt="aouros logo" />
+</div>
+
+
+<style>
+    :global(html), :global(body) {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        background-color: var(--color-primary);
+        background-image: none;
+    }
+
+    .placer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    img {
+        width: 40vh;
+        max-width: 40vh;
+        max-height: 30vh;
+        aspect-ratio: 1/1;
+    }
+</style>
